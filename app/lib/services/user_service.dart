e2c91dbba7c1757429b729e92e4664175bf9a664
SirusCodes <darshandrander@gmail.com>
2020-07-04 01:04:11 +0530

    added services

---
+++ app/lib/services/user_service.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../models/user/user.dart';
+import '../utils/setup_locator.dart';
+import 'shared_prefs.dart';
+
+class UserService {
+  final Firestore _db = Firestore.instance;
+  final SharedPrefs user = locator<SharedPrefs>();
+  CollectionReference ref;
+
+  UserService() {
+    ref = _db.collection("users");
+  }
+
+  Future<DocumentSnapshot> getUserData() async {
+    return ref.document(user.authID).get();
+  }
+
+  Future<void> setUserData(User userModel) {
+    return ref.document(user.authID).setData(userModel.toJson());
+  }
+}
