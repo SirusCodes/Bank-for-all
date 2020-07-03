4c5633322d97f29feea3c4b8fa2710257502a9a7
SirusCodes <darshandrander@gmail.com>
2020-07-03 21:54:32 +0530

    login with google add

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 import 'screens/login_screen/login_screen.dart';
-
+import 'utils/setup_locator.dart';
 import 'utils/theme.dart';
 
 void main() {
-  // setupLocator();
+  setupLocator();
   runApp(MyApp());
 }
 
--- app/lib/screens/login_screen/login_screen.dart
+++ app/lib/screens/login_screen/login_screen.dart
@@ -1,3 +1,6 @@
+import 'package:bank_for_all/screens/fingerprint_screen/fingerprint_screen.dart';
+import 'package:bank_for_all/services/auth_service.dart';
+import 'package:bank_for_all/utils/setup_locator.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -12,8 +15,26 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
+        child: Center(
+          child: InkWell(
+            onTap: () => locator<AuthService>()
+                .signInWithGoogle()
+                .catchError((e) {})
+                .then(
+                  (value) => Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => const FingerPrintScreen())),
+                ),
+            borderRadius: BorderRadius.circular(30),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Image.asset(
+                "assets/images/google_logo.jpg",
+                height: 100,
+              ),
+            ),
+          ),
         ),
       ),
     );
