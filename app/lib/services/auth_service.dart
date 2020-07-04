fb04d671d010efcb4e925e10af3fde5b3aa7096d
SirusCodes <darshandrander@gmail.com>
2020-07-04 21:01:16 +0530

    user data sending to firestore

--- app/lib/services/auth_service.dart
+++ app/lib/services/auth_service.dart
@@ -1,3 +1,8 @@
+import 'dart:math';
+
+import 'package:bank_for_all/enums/gender_enum.dart';
+import 'package:bank_for_all/models/user/user.dart';
+import 'package:bank_for_all/services/user_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -9,12 +14,38 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final GoogleSignIn _googleSignIn = GoogleSignIn();
 
+  final _userService = locator<UserService>();
+
   void _userFromFirebase(FirebaseUser user) {
     locator<SharedPrefs>().setUser(
       user.displayName,
       user.email,
       user.uid,
     );
+
+    final username = user.displayName.split(" ");
+
+    _userService.setUserData(User(
+      id: user.uid,
+      balance: "1000",
+      firstName: username[0],
+      lastName: username[1],
+      gender: GenderEnum.male,
+      gmail: user.email,
+      ifscCode: "BNK00000707",
+      phone: user.phoneNumber,
+      accountNumber: makeAccountNumber(),
+    ));
+  }
+
+  String makeAccountNumber() {
+    String number = "";
+    for (var i = 0; i < 12; i++) {
+      // ignore: use_string_buffers
+      number += Random().nextInt(9).toString();
+    }
+
+    return number;
   }
 
   Future<bool> signInWithGoogle() async {
